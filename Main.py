1edaa07fc12c2f1d878370440615c9fc7775718a
volkan-a <vakkaya@gmail.com>
2022-11-03 11:28:15 +0300

    Initial commit

---
+++ Main.py
@@ -0,0 +1,17 @@
+"""
+Main page for multipage streamlit thermodynamics demo app
+"""
+import streamlit as st
+
+st.set_page_config(page_title="Thermodynamics Demos", page_icon=":fire:")
+
+st.write("# Thermodynamics Demos")
+st.sidebar.success("Select a demo from the sidebar")
+
+
+st.markdown(
+    """
+    ## Introduction
+    More to come...
+    """
+)
